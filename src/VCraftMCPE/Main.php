36adc21efa7854cd745b8bcab8aa88ee4ca7a98d
VCraftMCPE <vcraftmcpe223@gmail.com>
2017-12-28 21:27:17 +0200

    Update Main.php

--- src/VCraftMCPE/Main.php
+++ src/VCraftMCPE/Main.php
@@ -131,9 +131,6 @@ class Main extends PluginBase implements Listener{
         $form = $api->createCustomForm(function (Player $event, array $data){
             $result = $data[1];
             $player = $event->getPlayer();
-          if($result != null){
-                $player->sendMessage($result);
-              }
          });
        $form->setTitle("Buy enchantment");
        $form->addSlider("Level", 1, 5, -1, -1);
